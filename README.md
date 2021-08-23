1077ad9caa512b67a33d9eba874e47fafd957106
rajckamariya <r.c.kamariya@gmail.com>
2021-08-23 11:13:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-lwt1of
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-lwt1of)
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './servers/server/server.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+import { ServersService } from './servers/servers.service';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'users/:id', component: UserComponent },
+  { path: 'servers', component: ServersComponent }
+];
+@NgModule({
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ServersComponent,
+    ServerComponent,
+    EditServerComponent,
+    UsersComponent,
+    UserComponent
+  ],
+  providers: [ServersService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
