2dfcbed7e53d3fc480854122c228332e7a1a5d71
rajckamariya <r.c.kamariya@gmail.com>
2021-08-23 12:09:06 +0530

    2 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,7 +14,7 @@ export class HomeComponent implements OnInit {
 
   onLoadServer(id: number) {
     this.router.navigate(['/servers', id, 'edit'], {
-      queryParams: { allowEdit: 1 },
+      queryParams: { allowEdit: '1' },
       fragment: 'loading'
     });
   }
